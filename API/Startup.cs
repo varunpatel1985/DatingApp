1bf3df125f4bf9cbc724f440ff7a7f1953abdc3b
varun patel <varunpatel1985@gmail.com>
2021-07-03 18:25:48 +1000

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -58,7 +59,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
